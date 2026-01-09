20a827db8991b45591049169fff196c792b5e351
Francisco Lizcano <flizcanor@icloud.com>
2026-01-09 18:33:54 -0500

    ajustes

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -180,7 +180,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/aCbu1eHlc90?si=UNlX0q1XdZQCTbL9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen) 
     
     .bg-full-width.bg-color-8
       .px-4.px-md-5.pb-md-3
